dd191a003e25050913649c36c619241c5c86d1ff
Smith_Project <capranov922@yahoo.com>
2020-07-21 21:44:31 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BindingList<Class1> _todoData;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoData = new BindingList<Class1>() {
+                new Class1(){ Text="test1"},
+                new Class1(){ Text="test2"},
+                new Class1(){ Text="test2" , IsDone  = true}
+            };
+
+            dgTodoList.ItemsSource = _todoData;
+            _todoData.ListChanged += _todoDataList_ListChanged;
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e) {
+
+
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+
+            }
+
+            switch (e.ListChangedType) {
+
+                case ListChangedType.Reset:
+                    break;
+                case ListChangedType.ItemAdded:
+                    break;
+                case ListChangedType.ItemMoved:
+                    break;
+                case ListChangedType.PropertyDescriptorAdded:
+                    break;
+                case ListChangedType.PropertyDescriptorDeleted:
+                    break;
+                case ListChangedType.PropertyDescriptorChanged:
+                    break;
+                default:
+                    break; 
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    class Class1
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+        private string _text;
+
+        public bool IsDone {
+            get { return _isDone; }
+            set {
+                if (_isDone ==value)
+                    return;
+
+                _isDone = value;
+
+                onPropertyChanged("IsDone");
+
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set {
+
+                if (_text == value)
+                    return;
+                
+                _text = value;
+                onPropertyChanged("Text");
+            
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void onPropertyChanged(string propertyName = "")
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
